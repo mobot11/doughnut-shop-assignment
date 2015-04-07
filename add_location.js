93487d8e0b119607fb444cbe4ff83208e74382cb
Dylan T. Moberg <dylanmoberg@msn.com>
2015-04-07 16:38:00 -0700

    got code to push total days doughnut sales to table

--- add_location.js
+++ add_location.js
@@ -8,27 +8,26 @@
     this.donutArray = [];
   };
   UserLocation.prototype.customersPerHour = function () {
-    return console.log('this is the customers per hour function ' + Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) +
-      this.minCustomers));
+    return  + Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) +
+      this.minCustomers);
   };
 
   UserLocation.prototype.donutsPerHour = function () {
     return this.customersPerHour() * this.averageCustomers;
   };
 
-  UserLocation.prototype.donutsPerDay = function() {
+UserLocation.prototype.donutsPerDay = function() {
     var total = 0;
     var perHour;
 
-    for (var i = 0; i <= this.hoursOpen; i++){
-    perHour = this.donutsPerHour();
-    total += perHour;
-    console.log(total)
-  }
- // console.log('this is the total' + total);
-
-  return total;
+    for (var i = 0; i <= 11; i++) {
+      perHour = this.donutsPerHour();
+      total += perHour;
+      console.log('total ' + total);
+    }
+    return total;
   };
+
   UserLocation.prototype.render = function () {
     var el = document.createElement('td')
     var tableEl = document.getElementById('donutTable')
